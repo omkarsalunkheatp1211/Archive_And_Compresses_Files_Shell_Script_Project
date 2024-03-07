c7b9adf0f9a456b5e0faf7069583c85ffe7d2010
Omkar Salunkhe <96873232+omkarsalunkheatp1211@users.noreply.github.com>
2024-03-07 23:46:11 +0530

    Add files via upload

---
+++ archive_project.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Variables
+BASE="/home/devops/myscripts/projects/new"
+DAYS=10
+DEPTH=1
+RUN=0
+
+# Check if the directory is present or not
+if [ ! -d "$BASE" ]; then
+    echo "Directory does not exist: $BASE"
+    exit 1
+fi
+
+# Create 'archive' folder if not present
+if [ ! -d "$BASE/archive" ]; then
+    mkdir "$BASE/archive"
+fi
+
+# Find the list of files larger than 20MB
+# Use command substitution to get the list of files
+# Enclose the find command in $() for command substitution
+for i in $(find "$BASE" -maxdepth "$DEPTH" -type f -size +20M); do
+    if [ $RUN -eq 0 ]; then
+        echo "[$(date "+%Y-%m-%d %H:%M:%S")] archiving $i ==> $BASE/archive"
+        gzip "$i" || exit 1
+        mv "$i.gz" "$BASE/archive" || exit 1
+    fi
+done
